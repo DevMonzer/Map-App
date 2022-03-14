182a668989ce6ca243a58c5b05fb8f0eec92aff7
Monzer Mohammad <dev.monzer@gmail.com>
2022-03-14 22:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if (navigator.geolocation)
 
       const map = L.map('map').setView(coords, 13);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
