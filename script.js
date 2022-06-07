230249196ff666dfba266846333cc0925d5b1d86
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-07 13:19:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,13 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
   }
 }
 
