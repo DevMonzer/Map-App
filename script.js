78e320a40c636b043e1110d527cf621c897b8183
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-07 13:19:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
   }
 }
 
